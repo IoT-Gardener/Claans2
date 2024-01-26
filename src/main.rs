ffc7e1884ba3a2acb072430072f7b30f1c72be6a
whompratt <JakeR1342@gmail.com>
2024-01-26 14:14:55 +0000

    Restructured templates, added web-based org creation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 extern crate rocket;
 
-use rocket::{launch, routes};
-use rocket_dyn_templates::Template;
-mod services;
+use rocket::{launch, routes, get};
+use rocket_dyn_templates::{context, Template};
 pub mod models;
 pub mod schema;
+mod services;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("pages/index", context! {})
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![services::create_org])
+        .mount("/", routes![index])
+        .mount("/", routes![services::create_org_json])
+        .mount("/", routes![services::create_org_form])
         .mount("/", routes![services::list_orgs])
         .attach(Template::fairing())
 }
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,23 +1,25 @@
 extern crate diesel;
 extern crate rocket;
 
+use crate::models;
+use crate::schema;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use rocket::response::{status::Created, Debug};
 use rocket::serde::{json::Json, Deserialize, Serialize};
-use rocket::{get, post};
-use crate::models;
-use crate::schema;
+use rocket::{get, post, FromForm, form::Form};
 use rocket_dyn_templates::{context, Template};
 use std::env;
+use crate::models::Org;
 
 type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
 
-#[derive(Serialize, Deserialize, Insertable)]
+#[derive(Serialize, Deserialize, Insertable, FromForm)]
 #[diesel(table_name = schema::orgs)]
 pub struct NewOrg {
-    org_name: String
+    #[field(validate = len(1..))]
+    org_name: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -41,7 +43,7 @@ pub fn establish_connection_pg() -> PgConnection {
 }
 
 #[post("/orgs", format = "json", data = "<org>")]
-pub fn create_org(org: Json<NewOrg>) -> Result<Created<Json<NewOrg>>> {
+pub fn create_org_json(org: Json<NewOrg>) -> Result<Created<Json<NewOrg>>> {
     use self::schema::orgs::dsl::*;
     let connection = &mut establish_connection_pg();
 
@@ -56,6 +58,28 @@ pub fn create_org(org: Json<NewOrg>) -> Result<Created<Json<NewOrg>>> {
     Ok(Created::new("/").body(org))
 }
 
+#[post("/orgs", format = "multipart/form-data", data = "<org>")]
+pub fn create_org_form(org: Form<NewOrg>) -> Template {
+    use self::schema::orgs::dsl::*;
+    use self::models::Org;
+
+    let connection = &mut establish_connection_pg();
+
+    let new_org = NewOrg {
+        org_name: org.org_name.to_string(),
+    };
+
+    diesel::insert_into(orgs)
+        .values(&new_org)
+        .execute(connection)
+        .expect("Error creating new org.");
+
+    let results = self::schema::orgs::dsl::orgs
+        .load::<Org>(connection)
+        .expect("Error loading orgs.");
+    Template::render("pages/orgs", context! {orgs: &results, org_count: results.len()})
+}
+
 #[get("/orgs")]
 pub fn list_orgs() -> Template {
     use self::models::Org;
@@ -63,5 +87,5 @@ pub fn list_orgs() -> Template {
     let results = self::schema::orgs::dsl::orgs
         .load::<Org>(connection)
         .expect("Error loading orgs.");
-    Template::render("orgs", context! {orgs: &results, count: results.len()})
+    Template::render("pages/orgs", context! {orgs: &results, org_count: results.len()})
 }
