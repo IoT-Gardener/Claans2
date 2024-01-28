917fdd2f6694abf55748b416910598b1ae4eb8d3
whompratt <JakeR1342@gmail.com>
2024-01-28 01:24:26 +0000

    Added active_quests table

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    active_quests (id) {
+        id -> Int4,
+        description -> Nullable<Text>,
+        reward -> Text,
+        is_active -> Bool,
+        quest_definition_id -> Int4,
+        org_id -> Int4,
+    }
+}
+
+diesel::table! {
     claans (id) {
         id -> Int4,
         name -> Text,
@@ -33,9 +44,17 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(active_quests -> orgs (org_id));
+diesel::joinable!(active_quests -> quest_definitions (quest_definition_id));
 diesel::joinable!(claans -> orgs (org_id));
 diesel::joinable!(quest_definitions -> orgs (org_id));
 diesel::joinable!(users -> claans (claan_id));
 diesel::joinable!(users -> orgs (org_id));
 
-diesel::allow_tables_to_appear_in_same_query!(claans, orgs, quest_definitions, users,);
+diesel::allow_tables_to_appear_in_same_query!(
+    active_quests,
+    claans,
+    orgs,
+    quest_definitions,
+    users,
+);
